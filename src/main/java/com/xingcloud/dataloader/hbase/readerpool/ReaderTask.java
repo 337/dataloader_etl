176e4d7662763bbd24dbf77a2acf4105a938fdf4
Maslino <liuxionghust@gmail.com>
2013-10-25 18:14:22 +0800

    set reset cache interval to flush interval

--- src/main/java/com/xingcloud/dataloader/hbase/readerpool/ReaderTask.java
+++ src/main/java/com/xingcloud/dataloader/hbase/readerpool/ReaderTask.java
@@ -122,7 +122,8 @@ public class ReaderTask implements Runnable {
         SeqUidCacheMap.getInstance().flushCacheToLocal(project);
 
       //每天检查一次该项目的缓存是否需要重置
-      if (index == flushInternal * 3.5)
+//      if (index == flushInternal * 3.5)
+      if (index % flushInternal == 0)
         SeqUidCacheMap.getInstance().resetPidCache(project);
 
       LOG.info("finish reading and flushing events the project :" + project + " using " + this.timeTotal + " ms," +
