07d066e309c5a06cba7101e766ffcfc16d55d94b
liqiang <lq0987>
2015-07-02 12:55:34 +0800

    change to bloomfilter

--- src/main/java/com/xingcloud/dataloader/hbase/table/user/Bitmap.java
+++ src/main/java/com/xingcloud/dataloader/hbase/table/user/Bitmap.java
@@ -1,6 +1,7 @@
 package com.xingcloud.dataloader.hbase.table.user;
 
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Random;
 
 /**
@@ -18,14 +19,24 @@ public class Bitmap {
 
   private long lower = -1l; //a reference value, decide if expand to lower or expand to upper
 
+  private BloomFilter<Long> bloomFilter = new BloomFilter<Long>(0.001, 1000000);
+
   public boolean get(long id) {
+    return bloomFilter.contains(id);
+  }
+
+/*  public boolean get(long id) {
     if (lower < 0)
       return false;
     int offset = (int) ((id - lower) >> 3);
     return !(offset >= bits.length || offset < 0) && (bits[offset] & masks[((int) (id & 7))]) != 0;
-  }
+  }*/
 
   public void set(long id, boolean is) {
+    bloomFilter.add(id);
+  }
+
+  public void setX(long id, boolean is) {
     if (lower < 0)
       lower = id;    //init  lower
     if (id >= lower && id < lower + (bits.length << 3)) {
@@ -77,6 +88,7 @@ public class Bitmap {
   }
 
   public void reset() {
-    Arrays.fill(bits, (byte) 0);
+//    Arrays.fill(bits, (byte) 0);
+    bloomFilter.clear();
   }
 }
