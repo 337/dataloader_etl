b5c2d11190fc064af45bcd333c334ec865a1dd8e
liqiang <lq0987>
2015-07-03 22:24:08 +0800

    debug

--- src/main/java/com/xingcloud/dataloader/server/DataLoaderNodeTask.java
+++ src/main/java/com/xingcloud/dataloader/server/DataLoaderNodeTask.java
@@ -225,8 +225,8 @@ public class DataLoaderNodeTask implements Runnable, Comparable<DataLoaderNodeTa
     }
 
     //特殊处理，将处理时间长的项目排在前面
-    String[] largeProjects = new String[]{"sof-isafe","sof-yacnvd","cok","cokfb","age","delta-homes","security-protection","22find","sof-installer","sweet-page",
-            "sof-wpm","sof-zip","sof-ient","mystartsearch","v9","webssearches","infospace","omiga-plus"};
+    String[] largeProjects = new String[]{"quick-start","sof-isafe","sof-px","firefox-searchengine","sof-yacnvd","cok","cokfb","age","delta-homes","security-protection","22find","sof-installer","sweet-page",
+            "sof-wpm","sof-zip","sof-ient","mystartsearch","v9","webssearches","infospace","omiga-plus","searchprotect"};
 
     for(String project: largeProjects){
       if(projectSet.contains(project)){
